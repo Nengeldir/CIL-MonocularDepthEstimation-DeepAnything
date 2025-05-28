41fbd9976c3dc5189673ca00fbeb61b549f78477
MarinoEisenegger <74112851+MarinoEisenegger@users.noreply.github.com>
2025-05-28 17:43:29 +0200

    Add AdvancedUnet Models

--- src/models.py
+++ src/models.py
@@ -53,4 +53,147 @@ class SimpleUNet(nn.Module):
         x = torch.sigmoid(x)*10
         
         return x
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
     
+    def forward(self, x):
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.relu2(self.bn2(self.conv2(x)))
+        return x
+    
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels, resizing):
+        super(Down, self).__init__()
+        self.MP = nn.MaxPool2d(resizing)
+        self.conv = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        return self.conv(self.MP(x))
+    
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, resizing):
+        super(Up, self).__init__()
+        self.up = nn.Upsample(scale_factor=resizing, mode='bilinear', align_corners=True)
+        self.conv = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class AdvancedUNet(nn.Module):
+    def __init__(self):
+        super(AdvancedUNet, self).__init__()
+
+        self.inc = (ConvBlock(3, 64))
+        self.down1 = (Down(64, 128, 2))
+        self.down2 = (Down(128, 256, 2))
+        self.down3 = (Down(256, 512, 2))
+
+        factor = 2
+
+        self.down4 = (Down(512, 1024 // factor, 2))
+        self.up1 = (Up(1024, 512 // factor, 2))
+        self.up2 = (Up(512, 256 // factor, 2))
+        self.up3 = (Up(256, 128 // factor, 2))
+        self.up4 = (Up(128, 64, 2))
+
+        self.final = nn.Conv2d(64, 1, kernel_size=1)
+    
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.final(x)
+        x = torch.sigmoid(x)*10
+        return x
+
+class DownVar(nn.Module):
+    def __init__(self, in_channels, out_channels, resizing):
+        super(DownVar, self).__init__()
+        self.MP = nn.MaxPool2d(resizing)
+        self.conv = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        return self.conv(self.MP(x))
+
+
+class UpVar(nn.Module):
+    def __init__(self, in_channels, out_channels, resizing):
+        super(UpVar, self).__init__()
+        self.up = nn.Upsample(scale_factor=resizing, mode='bilinear', align_corners=True)
+        self.conv = ConvBlock(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+class AdvancedUNetPooling(nn.Module):
+    def __init__(self):
+        super(AdvancedUNetPooling, self).__init__()
+
+        self.inc = (ConvBlock(3, 64))
+        self.down1 = (Down(64, 128, 8))
+        self.down2 = (Down(128, 256, 4))
+        self.down3 = (Down(256, 512, 4))
+
+        factor = 2
+
+        self.down4 = (Down(512, 1024 // factor, 2))
+        self.up1 = (Up(1024, 512 // factor, 2))
+        self.up2 = (Up(512, 256 // factor, 4))
+        self.up3 = (Up(256, 128 // factor, 4))
+        self.up4 = (Up(128, 64, 8))
+
+        self.final = nn.Conv2d(64, 1, kernel_size=1)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        x = self.final(x)
+        x = torch.sigmoid(x) * 10
+        return x
